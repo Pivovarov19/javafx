cea6ce0fd5c19223c9df7ece1d1323f3d617afd7
Andrew Pivovarov <pib2703@gmail.com>
2019-12-17 19:26:17 +0300

    added Calculator

---
+++ src/Pivovarov19/calculator/CalculatorController.java
@@ -0,0 +1,28 @@
+package Pivovarov19.calculator;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+
+    @FXML
+    TextField Field;
+
+    @FXML
+    /*public void onLogInClicked(Event event) {
+        System.out.println(logId.getText() + ":   " + logIn.getText());
+        System.out.println(passId.getText() + ":   " + password.getText());
+        String userLogin = "pipipi";
+        String userPass = "123123";
+        if(logIn.getText().equals(userLogin) && password.getText().equals(userPass)) {
+            System.out.println("Regular password");
+        } else {
+            System.out.println("Irregular password/login");
+        }
+    }*/
+
+}
