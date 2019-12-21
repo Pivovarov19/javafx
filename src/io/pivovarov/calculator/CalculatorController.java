d5731f2b466f5ed84c97415bac749db10c9e5bbc
Andrew Pivovarov <pib2703@gmail.com>
2019-12-21 15:34:10 +0300

    controller: add code to controller, done all tasks except last 3 tasks

--- src/io/pivovarov/calculator/CalculatorController.java
+++ src/io/pivovarov/calculator/CalculatorController.java
@@ -1,28 +1,48 @@
 package io.pivovarov.calculator;
 
-import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class CalculatorController {
+    @FXML
+    public TextField field;
 
     @FXML
-    TextField Field;
-
-    //@FXML
-    /*public void onLogInClicked(Event event) {
-        System.out.println(logId.getText() + ":   " + logIn.getText());
-        System.out.println(passId.getText() + ":   " + password.getText());
-        String userLogin = "pipipi";
-        String userPass = "123123";
-        if(logIn.getText().equals(userLogin) && password.getText().equals(userPass)) {
-            System.out.println("Regular password");
-        } else {
-            System.out.println("Irregular password/login");
+    public void onMouseClickListener(MouseEvent mouseEvent) {
+        if (mouseEvent.getSource() instanceof Button) {
+            Button button = (Button) mouseEvent.getSource();
+            System.out.println("Pressed button: " + button.getText());
+            if (button.getText().equals("CE")){
+                field.setText("");
+            } else {
+                field.setText(field.getText() + button.getText());
+            }
         }
-    }*/
+    }
 
+    @FXML
+    public void onKeyPressedListener(KeyEvent keyEvent) {
+        switch (keyEvent.getText()) {
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+            case "+":
+            case "-":
+            case "/":
+            case "*":
+            case "=":
+                System.out.println("Pressed button: " + keyEvent.getText());
+                field.setText(field.getText() + keyEvent.getText());
+        }
+    }
 }
