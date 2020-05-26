3ffbc929f5a7ab63160637acf11fe54ae864c29e
Andrew Pivovarov <pib2703@gmail.com>
2020-05-26 18:14:12 +0300

    Solution: Working Game

--- src/by/pivovarov/Pivovarov19/tictactoe/ControllerTicTacToe.java
+++ src/by/pivovarov/Pivovarov19/tictactoe/ControllerTicTacToe.java
@@ -7,6 +7,7 @@ import javafx.scene.input.MouseEvent;
 
 public class ControllerTicTacToe {
     boolean isX = true;
+    int i;
     //ModelTicTacToe model = new ModelTicTacToe();
     @FXML
     Label turnOutput;
@@ -33,19 +34,25 @@ public class ControllerTicTacToe {
     @FXML
     Button bottomRight;
 
+
     @FXML
     public void onMouseClickListener(MouseEvent mouseEvent) {
         if (mouseEvent.getSource() instanceof Button) {
             Button button = (Button) mouseEvent.getSource();
+            if (i == 0) {
+                turnOutput.setText("X");
+            }
             if (button.getText() == "") {
-                if (isX == true) {
+                if (turnOutput.getText().equals("X")) {
                     button.setText("X");
-                    isX = false;
+                    //isX = false;
                     turnOutput.setText("0");
+                    i++;
                 } else {
                     button.setText("0");
-                    isX = true;
+                    //isX = true;
                     turnOutput.setText("X");
+                    i++;
                 }
                 //buttonPressed(button.getId());
             } else if (button.getId().equals("restartButton")) {
@@ -59,25 +66,29 @@ public class ControllerTicTacToe {
                 bottom.setText("");
                 bottomRight.setText("");
                 display.setText("Welcome to Tic-Tac-Toe!");
-                isX = true;
-                turnOutput.setText("X");
+                //isX = true;
+                i = 0;
             }
             System.out.println("Pressed button: " + button.getId());
             // Win Controller
-            if ((upLeft.getText().equals(up.getText()) && upLeft.getText().equals(upRight.getText())) // up floor
-                    || (upLeft.getText().equals(left.getText()) && upLeft.getText().equals(bottomLeft.getText())) // left colomn
-                    || (upLeft.getText().equals(center.getText()) && upLeft.getText().equals(bottomRight.getText())) // upLeft to bottomRight diagonal
-                    || (up.getText().equals(center.getText()) && up.getText().equals(bottom.getText())) // center colomn
-                    || (upRight.getText().equals(center.getText()) && upRight.getText().equals(bottomLeft.getText())) // upRight to bottomLeft diagonal
-                    || (upRight.getText().equals(right.getText()) && upRight.getText().equals(bottomRight.getText())) // right colomn
-                    || (left.getText().equals(center.getText()) && left.getText().equals(right.getText())) // center floor
-                    || (bottomLeft.getText().equals(bottom.getText()) && bottomLeft.getText().equals(bottomRight.getText()))) {  // bottom floor
-                if (isX == true) {
+            if ((upLeft.getText().equals(up.getText()) && upLeft.getText().equals(upRight.getText()) && !upLeft.getText().equals("")) // up floor
+                    || (upLeft.getText().equals(left.getText()) && upLeft.getText().equals(bottomLeft.getText()) && !upLeft.getText().equals("")) // left colomn
+                    || (upLeft.getText().equals(center.getText()) && upLeft.getText().equals(bottomRight.getText()) && !upLeft.getText().equals("")) // upLeft to bottomRight diagonal
+                    || (up.getText().equals(center.getText()) && up.getText().equals(bottom.getText()) && !up.getText().equals("")) // center colomn
+                    || (upRight.getText().equals(center.getText()) && upRight.getText().equals(bottomLeft.getText()) && !upRight.getText().equals("")) // upRight to bottomLeft diagonal
+                    || (upRight.getText().equals(right.getText()) && upRight.getText().equals(bottomRight.getText()) && !upRight.getText().equals("")) // right colomn
+                    || (left.getText().equals(center.getText()) && left.getText().equals(right.getText()) && !left.getText().equals("")) // center floor
+                    || (bottomLeft.getText().equals(bottom.getText()) && bottomLeft.getText().equals(bottomRight.getText())) && !bottomLeft.getText().equals("")) {  // bottom floor
+                if (turnOutput.getText().equals("X")) {
                     display.setText("0 WINS, restart the game");
 
-                } else if(isX == false) {
+                } else if(turnOutput.getText().equals("0")) {
                     display.setText("X WINS, restart the game");
                 }
+            } else if(!upLeft.getText().equals("") && !up.getText().equals("") && !upRight.getText().equals("")
+                    && !left.getText().equals("") && !center.getText().equals("") && !right.getText().equals("")
+                    && !bottomLeft.getText().equals("") && !bottom.getText().equals("") && !bottomRight.getText().equals("")){
+                display.setText("Round Draw, restart the game");
             }
         }
 
